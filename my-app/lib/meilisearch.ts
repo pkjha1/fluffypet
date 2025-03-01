5e3050721bab7014621553916f3e704899a0b2bd
pkjha1 <prashantcool28@gmail.com>
2025-03-01 09:20:02 +0530

    Delete my-app directory

--- my-app/lib/meilisearch.ts
+++
@@ -1,9 +0,0 @@
-import { MeiliSearch } from 'meilisearch'
-
-const client = new MeiliSearch({
-  host: process.env.MEILISEARCH_URL || 'http://localhost:7700',
-  apiKey: process.env.MEILISEARCH_ADMIN_KEY,
-})
-
-export default client
-
